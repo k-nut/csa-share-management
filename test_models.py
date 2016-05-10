1454912989782f5d6a797b7b8a8f3477eab349fa
Knut Hühne <knut@k-nut.eu>
2016-05-10 11:33:50 +0200

    fix tests

--- test_models.py
+++ test_models.py
@@ -1,44 +1,54 @@
-from flask.ext.testing import TestCase
-from flask import Flask
+import unittest
+import tempfile
+import os
 
-from app import db
+from solawi import app, db
 
-class MyTest(TestCase):
-    def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
-        return app
 
+class MyTest(unittest.TestCase):
     def setUp(self):
+        # next line needed so that db.create_all knows what to create
+        from models import Deposit, Share, Person
+
+        self.db_fd, self.temp_filepath = tempfile.mkstemp()
+        database_path = 'sqlite:///{}'.format(self.temp_filepath)
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        os.close(self.db_fd)
+        os.remove(self.temp_filepath)
 
     def test_person(self):
-        from models import Person, Deposit
+        from models import Person, Deposit, Share
         from datetime import date
 
-        p = Person("Knut Huehne")
+        person = Person.get_or_create("Knut Huehne")
 
         timestamp = date(2016, 1, 2)
         title = "Solawi Ratibor Knut Huehne"
         amount = 63.0
 
-        d = Deposit(title=title,
-                   person=p,
-                   amount=amount,
-                   timestamp=timestamp)
+        deposit = Deposit(title=title,
+                          person=person,
+                          amount=amount,
+                          timestamp=timestamp)
 
-        d.save()
-        assert p.number_of_deposits == 1
-        assert p.total_deposits == 63
+        deposit.save()
+
+        share = Share("Share number one")
+        share.people.append(person)
+        share.save()
+        assert share.number_of_deposits == 1
+        assert share.total_deposits == 63
         assert len(Deposit.query.all()) == 1
-
-        d.save()
 
     def test_other(self):
         from models import Deposit
+        print Deposit.query.all()
         assert len(Deposit.query.all()) == 0
