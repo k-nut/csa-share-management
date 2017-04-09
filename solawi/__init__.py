7f986702cfcc4a80fdf9df70ef428b6befaafe60
Knut Hühne <knut@k-nut.eu>
2017-04-09 22:33:24 +0200

    use new way of referencing extensions

--- solawi/__init__.py
+++ solawi/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
