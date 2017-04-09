d9bfe39da1966c9305fb4bf0ad02caf4ab0e67e2
Knut Hühne <knut@k-nut.eu>
2017-04-09 19:19:31 +0200

    make database configurable

--- solawi/__init__.py
+++ solawi/__init__.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/knut/solawi.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:////home/knut/solawi.db')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
