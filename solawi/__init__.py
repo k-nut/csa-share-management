c82cc4da70874a80a11bc5c787462fae46058495
Knut Hühne <knut@k-nut.eu>
2017-04-09 19:35:00 +0200

    read debug setting from env variables

--- solawi/__init__.py
+++ solawi/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:////home/knut/solawi.db')
+app.debug = os.environ.get("DEBUG", False)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
