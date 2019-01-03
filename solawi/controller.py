902c8a2cf755edf009a81c4c5646a7c19cfc5080
Knut Hühne <knut@k-nut.eu>
2019-01-03 20:31:36 +0100

    make sure merging share persists multiple Members and Bets

--- solawi/controller.py
+++ solawi/controller.py
@@ -11,9 +11,9 @@ def merge(first_share_id, second_share_id):
     first_share = Share.query.get(first_share_id)
     second_share = Share.query.get(second_share_id)
 
-    first_share.people += second_share.people
-    first_share.members += second_share.members
-    first_share.bets += second_share.bets
+    first_share.people += list(second_share.people)
+    first_share.members += list(second_share.members)
+    first_share.bets += list(second_share.bets)
 
     first_share.save()
     second_share.delete()
--- test_controller.py
+++ test_controller.py
@@ -2,8 +2,9 @@ from solawi.app import db
 from test_factories import ShareFactory, MemberFactory, BetFactory
 from test_helpers import DBTest
 
-from solawi.models import Share, Bet
+from solawi.models import Share, Bet, Member
 from solawi.controller import merge
+
 
 class TestController(DBTest):
     def test_merge_one_way(self):
@@ -12,18 +13,24 @@ class TestController(DBTest):
 
         MemberFactory.create(share=share1)
         MemberFactory.create(share=share2)
+        MemberFactory.create(share=share2)
+
+        BetFactory.create(share=share1)
         BetFactory.create(share=share1)
         BetFactory.create(share=share2)
 
-        assert db.session.query(Share).count() == 2
-        assert Bet.query.count() == 2
+        assert Share.query.count() == 2
+        assert Bet.query.count() == 3
 
         merge(share1.id, share2.id)
 
-        updated_share = db.session.query(Share).first()
-        assert db.session.query(Share).count() == 1
-        assert len(updated_share.members) == 2
-        assert len(updated_share.bets) == 2
+        updated_share = Share.query.one()
+
+        assert Share.query.count() == 1
+        assert len(updated_share.members) == 3
+        assert len(updated_share.bets) == 3
+        assert Member.query.count() == 3
+        assert Bet.query.count() == 3
 
     def test_merge_other_way(self):
         share1 = ShareFactory.create()
@@ -31,15 +38,22 @@ class TestController(DBTest):
 
         MemberFactory.create(share=share1)
         MemberFactory.create(share=share2)
+        MemberFactory.create(share=share2)
+
+        BetFactory.create(share=share1)
         BetFactory.create(share=share1)
         BetFactory.create(share=share2)
 
-        assert db.session.query(Share).count() == 2
-        assert Bet.query.count() == 2
+
+        assert Share.query.count() == 2
+        assert Bet.query.count() == 3
 
         merge(share2.id, share1.id)
 
-        updated_share = db.session.query(Share).first()
-        assert db.session.query(Share).count() == 1
-        assert len(updated_share.members) == 2
-        assert len(updated_share.bets) == 2
+        updated_share = Share.query.one()
+
+        assert Share.query.count() == 1
+        assert len(updated_share.members) == 3
+        assert len(updated_share.bets) == 3
+        assert Member.query.count() == 3
+        assert Bet.query.count() == 3
