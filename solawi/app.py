222ee23316342a4e7eac62a4167d5b6875ac36c5
Knut Hühne <knut@k-nut.eu>
2021-04-05 12:59:39 +0200

    Rewrite Heroku-provided db string

--- solawi/app.py
+++ solawi/app.py
@@ -17,8 +17,17 @@ secret_key = os.environ.get('SECRET_KEY')
 if secret_key is None:
     raise Exception("You must supply the `SECRET_KEY` environment variable.")
 
+db_url = os.environ.get('DATABASE_URL')
+if db_url is None:
+    raise Exception("You must supply the `DATABASE_URL` environment variable.")
+if "postgres://" in db_url:
+    # The new SQLAlchemy versions expect the database name to be `postgresql`
+    # and not just `postgres`. Heroku will only provide `postgres` here though
+    # which is why we re-write it to match what SQLAlchemy expects.
+    db_url = db_url.replace("postgres://", "postgresql://")
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:////home/knut/solawi.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secret_key
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 60 * 60
